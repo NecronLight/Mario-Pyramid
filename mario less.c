35dd8456cc0bf0d52e1b2fd0d19f6f406f5d3057
Lucas da Luz Pereira <53914838+NecronLight@users.noreply.github.com>
2020-04-22 18:00:04 -0300

    Add files via upload

---
+++ mario less.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("What's the height of the pyramid?\n"); //ask for the pyramid height
+    }
+    while (height < 1 || height > 8);
+
+    for (int row = height; row >= 1 ; row--) // sets the row location
+    {
+        for (int spaces = 0; spaces < row - 1; spaces++) printf(" "); //sets number of spaces
+        for (int hashes = height; hashes >= row; hashes--) printf("#"); //sets number of hashes
+        printf("\n"); //prints space between
+    }
+}
